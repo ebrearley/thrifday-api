9e30a2606dc16fa42296fad9dcd132d12ef96e50
Eric Brearley <eric@ericbrearley.com>
2021-05-12 01:43:53 +1000

    Remove temp enum on User model

--- src/users/models/user.model.ts
+++ src/users/models/user.model.ts
@@ -1,6 +1,5 @@
 import { Field, ID, ObjectType } from '@nestjs/graphql';
 import { MonitoredProductModel } from '@products/models/monitored-product.model';
-import { RetailerEnum } from '@retailers/@enums/retailer.enum';
 import { UserEntity } from '@users/entities/user.entity';
 import { IsEmail } from 'class-validator';
 
@@ -13,9 +12,6 @@ export class UserModel {
   @IsEmail()
   email: string;
 
-  @Field(() => RetailerEnum, { nullable: true })
-  temp?: RetailerEnum;
-
   @Field((type) => [MonitoredProductModel], {
     nullable: true,
     defaultValue: [],
@@ -23,11 +19,9 @@ export class UserModel {
   monitoredProducts?: MonitoredProductModel[];
 
   static fromUserEntity(userEntity: UserEntity): UserModel {
-    const temp: RetailerEnum = RetailerEnum['Woolworths'];
     return {
       id: userEntity.id,
       email: userEntity.email,
-      temp,
     };
   }
 }
